de7b2d95a3e52e5661f65ee71bd7c59ff6e24121
Ataraxxia <fabulousmcc@gmail.com>
2022-01-20 22:55:33 +0100

    Unit tests

---
+++ godin_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "io/ioutil"
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+func TestGetDefaultPage(t *testing.T) {
+    req := httptest.NewRequest(http.MethodGet, "/", nil)
+    w := httptest.NewRecorder()
+    getDefaultPage(w, req)
+    res := w.Result()
+    defer res.Body.Close()
+    data, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        t.Errorf("expected error to be nil got %v", err)
+    }
+    if string(data) != "Godin" {
+        t.Errorf("expected Godin got %v", string(data))
+    }
+}
